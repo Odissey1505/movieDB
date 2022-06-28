f165688330e0b7af02ec376a449fe003aebefe5e
Taras <taraspisulinsky@gmail.com>
2022-06-29 01:11:14 +0300

    v1.1 Cycles & Conditions

--- js/script.js
+++ js/script.js
@@ -9,14 +9,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const   firstMovieName = prompt('Один з останніх переглянутих фільмів?', ''),
-        firstMovieMark = prompt('На скільки балів (з 10) оціните його?', ''),
-        secondMovieName = prompt('Один з останніх переглянутих фільмів?', ''),
-        secondMovieMark = prompt('На скільки балів (з 10) оціните його?', '');
+for (let i = 0; i < 2; i++) {
+    const   movieName = prompt('Один з останніх переглянутих фільмів?', ''),
+            movieMark = prompt('На скільки балів (з 10) оціните його?', '');
 
-personalMovieDB.movies[firstMovieName] = firstMovieMark;
-personalMovieDB.movies[secondMovieName] = secondMovieMark;
+    if (movieName != null && movieMark != null && movieName != '' && movieMark != '' && movieName.length < 50) {
+        personalMovieDB.movies[movieName] = movieMark;
+        console.log ('Done!');
+    } else {
+        console.log ('Fail. Try again');
+        i--;
+    }
+}
 
+if (personalMovieDB.count < 10) {
+    console.log('A little count of movies');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log ('You are classic movie watcher');
+} else if (personalMovieDB.count >= 30) {
+    console.log ('You are insane in movies');
+} else {
+    console.log ('Error');
+}
 console.log(personalMovieDB);
 
 
