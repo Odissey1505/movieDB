346362656e72c3aa67c21ceb968b23ca6c41d23f
Taras <taraspisulinsky@gmail.com>
2022-07-07 19:29:03 +0300

    Add functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 "use strict";
 
-const numberOfFilms = +prompt('Скільки фільмів Ви вже переглянули?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Скільки фільмів Ви вже переглянули?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Скільки фільмів Ви вже переглянули?', '');
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,28 +20,52 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const   movieName = prompt('Один з останніх переглянутих фільмів?', ''),
-            movieMark = prompt('На скільки балів (з 10) оціните його?', '');
 
-    if (movieName != null && movieMark != null && movieName != '' && movieMark != '' && movieName.length < 50) {
-        personalMovieDB.movies[movieName] = movieMark;
-        console.log ('Done!');
-    } else {
-        console.log ('Fail. Try again');
-        i--;
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        const   movieName = prompt('Один з останніх переглянутих фільмів?', ''),
+                movieMark = prompt('На скільки балів (з 10) оціните його?', '');
+    
+        if (movieName != null && movieMark != null && movieName != '' && movieMark != '' && movieName.length < 50) {
+            personalMovieDB.movies[movieName] = movieMark;
+            console.log ('Done!');
+        } else {
+            console.log ('Fail. Try again');
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('A little count of movies');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log ('You are classic movie watcher');
-} else if (personalMovieDB.count >= 30) {
-    console.log ('You are insane in movies');
-} else {
-    console.log ('Error');
+rememberMyFilms();
+
+
+function detectedPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log('A little count of movies');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log ('You are classic movie watcher');
+    } else if (personalMovieDB.count >= 30) {
+        console.log ('You are insane in movies');
+    } else {
+        console.log ('Error');
+    }
 }
-console.log(personalMovieDB);
+
+detectedPersonalLevel();
+
+function showMyDB (hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
 
 
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt (`Ваш улюблений жанр під номером ${i}`, '');
+    }
+}
+
+writeYourGenres();
